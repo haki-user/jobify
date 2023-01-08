92c245275fe2f93cc102195303e7749998c2c7ab
haki-user <adityapratapsingh33@outlook.com>
2023-01-08 14:14:11 +0530

    new file:   client/src/pages/dashboard.js 	deleted:    client/src/pages/dd.ttt 	new file:   client/src/pages/error.js 	new file:   client/src/pages/index.js 	new file:   client/src/pages/register.js

---
+++ client/src/pages/index.js
@@ -0,0 +1,7 @@
+import Dashboard from "./dashboard";
+import Landing from "./landing";
+import Error from "./error";
+import Register from "./register";
+
+
+export {Dashboard, Landing, Error, Register}
